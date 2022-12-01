05994dec8434170dc176694e2b2ab09714090b71
Jacob Brännström <jacob.brannstrom@gmail.com>
2022-12-01 14:42:52 +0100

    Update get_input.py

--- get_input.py
+++ get_input.py
@@ -12,6 +12,5 @@ response = requests.get(url= url, cookies= cookies)
 if response.ok:
     with open(f'{dayOfMonth}/input.txt', 'wb') as file:
         file.write(response.content)
-    
 else:
     print(f'/response.status_code {response.status_code}: {response.content}')
