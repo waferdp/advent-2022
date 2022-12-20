acd3a5a6b1d7205bdfaefcc566c05e8c2431610c
Jacob Brännström <jacob.brannstrom@gmail.com>
2022-12-20 10:02:27 +0100

    dec 16 part 1

--- 16/pressure.py
+++ 16/pressure.py
@@ -15,48 +15,39 @@ class Pressure:
         return graph
 
     def maxPressure(self):
-        return self.dfs(0, '', 0, 0, self.graph.get('AA'))
-
-    def getMostValuablePaths(self, a, opened):
-        paths = []
-        for path in self.graph.paths[a]:
-            leg = self.graph.paths[a][path]
-            rate = self.graph.get(path).rate
-            paths.append((path, rate/(len(leg)-1)) )
-        ordered = sorted(paths, key=lambda x: x[1], reverse=True)
-        return [node[0] for node in ordered]
+        return self.dfs(opened={}, current='AA')
         
-    
-    def dfs(self, minutes, opened, flowed, flow, current):
-        if minutes >= 30:
-            return flowed
+    def dfs(self, opened, current):
+        minutes = max(opened.values()) if len(opened) else 0
+        if minutes >= self.time:
+            return self.getFlow(opened)
         options = []
-        if current.rate > 0:
-            #opened.append(current.name)
-            opened += current.name
-            minutes += 1
-            flowed += flow
-            # flow increases in every following minute
-            flow += current.rate
-        ordered = self.getMostValuablePaths(current.name)
-        for path in ordered:
+        #ordered = self.getMostValuablePaths(current, opened)
+        for path in self.graph.paths[current]:
             if path in opened:
                 continue
-            leg = self.graph.paths[current.name][path]
-            time = len(leg)-1
+            leg = self.graph.paths[current][path]
+            time = len(leg)
             if minutes + time > self.time:
                 continue
             else:
-                option = self.dfs(minutes+time, opened, flowed + flow * time, flow, self.graph.get(path))
+                openCopy = opened.copy()
+                openTime = minutes+time
+                openCopy[path] = openTime 
+                option = self.dfs(openCopy, path)
                 options.append(option)
-                break
         if len(options):
             return max(options)
         else:
-            return (30-minutes)*flow + flowed
+            return self.getFlow(opened)
 
-    def double(self):
-        self.dfs(0, '', 0, 0, self.graph.get('AA'))
+    def getFlow(self, opened):
+        flowed = 0
+        for valve in opened:
+            rate = self.graph.get(valve).rate
+            openedAt = opened[valve]
+            flowed += rate * (self.time - openedAt)
+        return flowed
 
 if __name__ == '__main__':
     input = fileReader.read('input.txt')
